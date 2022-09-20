2fb1529a27a86e4419e136da7f0f17f9af4bb814
Jazmine <lm28810@gmail.com>
2022-09-20 15:44:48 -0400

    css 1st stage done

---
+++ src/Components/Advice/Advice.jsx
@@ -0,0 +1,15 @@
+import './Advice.css'
+import React from 'react'
+
+const Advice = () => {
+  return (
+      <div className='Advice' >
+          <div className='Advice-Card' >
+              <h1>Test</h1>
+              <button className='Advice-button' >Click for Advice</button>
+          </div>
+    </div>
+  )
+}
+
+export default Advice
