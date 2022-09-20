14636e4e51559832404344e276141578ea4af6f0
Jazmine <lm28810@gmail.com>
2022-09-20 16:13:35 -0400

    api put in place

--- src/Components/Advice/Advice.jsx
+++ src/Components/Advice/Advice.jsx
@@ -1,12 +1,25 @@
 import './Advice.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import React from 'react'
 
 const Advice = () => {
+    const [advice, setAdvice] = useState('')
+    useEffect(() => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+            console.log(response)
+            })
+            .catch((error) => {
+            console.log(error)
+        })
+    })
   return (
       <div className='Advice' >
+          
           <div className='Advice-Card' >
-              <h1>Test</h1>
-              <button className='Advice-button' >Click for Advice</button>
+              <h1>{advice} </h1>
+              <button className='Advice-button' onClick={setAdvice(data.slip)} >Click for Advice</button>
           </div>
     </div>
   )
