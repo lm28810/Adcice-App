ca11886481f0f60b8893ab5a84de478e6e2cf634
Jazmine <lm28810@gmail.com>
2022-10-01 19:41:52 -0400

    ready to deploy

--- src/Components/Advice/Advice.jsx
+++ src/Components/Advice/Advice.jsx
@@ -5,21 +5,40 @@ import React from 'react'
 
 const Advice = () => {
     const [advice, setAdvice] = useState('')
+   
+        
     useEffect(() => {
         axios.get('https://api.adviceslip.com/advice')
             .then((response) => {
-            console.log(response)
+                console.log(response)
+                const {advice} = response.data.slip
+                setAdvice(advice)
             })
             .catch((error) => {
             console.log(error)
         })
-    }, [])
+    }
+        , [])
+
+    function newAdvice() {
+            axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                console.log(response)
+                const {advice} = response.data.slip
+                setAdvice(advice)
+            })
+            .catch((error) => {
+            console.log(error)
+        })
+    }
+    
+
   return (
       <div className='Advice' >
           
           <div className='Advice-Card' >
               <h1>{advice} </h1>
-              <button className='Advice-button'  >Click for Advice</button>
+              <button className='Advice-button' onClick={newAdvice} >Click for Advice</button>
           </div>
     </div>
   )
